ae257b03b725117a3945a16e10514774a6678d1b
Hieu <hieu01672549298@gmail.com>
2023-03-30 17:05:25 +0700

    login and register

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModels');
 const asyncHandler = require('express-async-handler');
+const {generateToken} = require('../config/jwtToken')
 
 const createUser = asyncHandler(async (req, res, next)=>{
 	const email = req.body.email;
@@ -12,4 +13,21 @@ const createUser = asyncHandler(async (req, res, next)=>{
 	};
 });
 
-module.exports = {createUser};
+const loginUser = asyncHandler(async (req, res, next)=>{
+	const {email, password} = req.body;
+	const find = await User.findOne({email});
+	if(find && (await find.isPasswordMatched(password))){
+		res.json({
+			_id : find?._id,
+			firstName : find?.firstName,
+			lastName : find?.lastName,
+			email : find?.email,
+			mobile : find?.mobile,
+			token : generateToken(find?._id)
+		})
+	}else{
+		throw new Error("User is not match !!!");
+	};
+});
+
+module.exports = {createUser, loginUser};
--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); // Erase if already required
-
+const bcrypt = require('bcrypt'); // Erase if already required
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
     firstName:{
@@ -24,7 +24,20 @@ var userSchema = new mongoose.Schema({
 	type:String,
 	required:true,
     },
+    role:{
+	type:String,
+	default: "user",	
+    },
 });
+
+userSchema.pre("save", async function(next) {
+	const salt = bcrypt.genSaltSync(10);
+	this.password = await bcrypt.hashSync(this.password, salt);
+});
+
+userSchema.methods.isPasswordMatched = async function(enterPassword){
+	return await bcrypt.compare(enterPassword, this.password);
+};
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
